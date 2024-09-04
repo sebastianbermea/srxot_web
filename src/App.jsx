902717fce22db4946347906640707bfee26b7417
sebastianbermea <sebasbermea@hotmail.com>
2024-09-04 08:32:13 -0600

    Edit text

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import srxotlogo from './assets/srxotlogo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <img src={srxotlogo} className="logo" alt="Sr. Xot logo" />
       </div>
-      <h1>Proximamente...</h1>
+      <h1>Proximamente</h1>
      
     </>
   )
